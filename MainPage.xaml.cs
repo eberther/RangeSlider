d4c7687cc9d0e827b6ab321fb55a7b8eac0660e1
eberther <77499115+eberther@users.noreply.github.com>
2021-02-19 14:23:19 +0100

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.SfRangeSlider.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RangeSlider
+{   /// <summary>
+    /// Observable collection of Items list.
+    /// </summary>
+    public class ObservableCollectionList : ObservableCollection<Items>
+    {
+
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
